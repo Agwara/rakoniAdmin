300015a7d302a3a2bbacf9f3e1bca793942d2ba4
Agwara <nnaemekaagwara@gmail.com>
2021-12-06 23:53:50 +0100

    new update

--- src/components/Lecturer/LecturerList/Index.jsx
+++ src/components/Lecturer/LecturerList/Index.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react"
+import React, {useEffect, useState} from "react"
 
 import PageInfo from "../../PageInfo/PageInfo"
 import ListContent from "./ListContent"
@@ -16,13 +16,30 @@ const LecturerList = () => {
   const [searchString, setSearchString] = useState("")
   const [selectDropDown, setSelectDropDown] = useState(dropDownList[0])
 
+  const [filteredItems, setFilteredItems] = useState([...lectureData])
+
   const handleSearchChange = (e) => {
     setSearchString(e.target.value)
   }
 
-  const handleDropDownChange = () => {
-
+  const handleDropDownChange = (e) => {
+    setSelectDropDown(e.target.value)
   }
+
+  const handleFilterChange = () => {
+    let tempDataOne = lectureData.filter((item) => item.fullName.toLowerCase().includes(searchString.toLowerCase()))
+
+    if (selectDropDown === "All") {
+      setFilteredItems([...tempDataOne])
+    } else {
+      let tempDataTwo = tempDataOne.filter((item) => item.faculty.toLowerCase().includes(selectDropDown.toLowerCase())) 
+      setFilteredItems([...tempDataTwo])
+    }
+  }
+
+  useEffect(() => {
+    handleFilterChange()
+  }, [searchString, selectDropDown])
 
   return (
     <div className={styles.container}>
@@ -56,14 +73,125 @@ const LecturerList = () => {
         </div>
       </div>
 
-      <ListContent />
+      <ListContent data={filteredItems} />
 
     </div>
   )
 }
 
 const dropDownList = [
-  "All", "Engineering", "Management", "Agriculture", "Sciences"
+  "All", "Engineering", "Management", "Agriculture", "Sciences", "Art"
+]
+
+const lectureData = [
+  {
+    "fullName": "Alonso Xabi",
+    "userID": "AlXabi",
+    "deparment": "Computer Science",
+    "email": "xabialonso@gmail.com",
+    "phone": "09065785432",
+    "role": "Lecturer 2",
+    "faculty": "Engineering"
+  },
+  {
+    "fullName": "Zenedine Zidane",
+    "userID": "Zizou",
+    "deparment": "Art",
+    "email": "zizouzidane@gmail.com",
+    "phone": "09076546542",
+    "role": "Doctor",
+    "faculty": "Art"
+  },
+  {
+    "fullName": "Andrea Pirlo",
+    "userID": "Maestro",
+    "deparment": "Architect",
+    "email": "thearchitect@gmail.com",
+    "phone": "09076546542",
+    "role": "Doctor",
+    "faculty": "Management"
+  },
+  {
+    "fullName": "Andreas Iniesta",
+    "userID": "Maestro",
+    "deparment": "Architect",
+    "email": "thearchitect@gmail.com",
+    "phone": "09076546542",
+    "role": "Doctor",
+    "faculty": "Management"
+  },
+  {
+    "fullName": "Alonso Xabi",
+    "userID": "AlXabi",
+    "deparment": "Computer Science",
+    "email": "xabialonso@gmail.com",
+    "phone": "09065785432",
+    "role": "Lecturer 2",
+    "faculty": "Engineering"
+  },
+  {
+    "fullName": "Zenedine Zidane",
+    "userID": "Zizou",
+    "deparment": "Art",
+    "email": "zizouzidane@gmail.com",
+    "phone": "09076546542",
+    "role": "Docter",
+    "faculty": "Management"
+  },
+  {
+    "fullName": "Andrea Pirlo",
+    "userID": "Maestro",
+    "deparment": "Architect",
+    "email": "thearchitect@gmail.com",
+    "phone": "09076546542",
+    "role": "Doctor",
+    "faculty": "Sciences" 
+  },
+  {
+    "fullName": "Andreas Iniesta",
+    "userID": "Maestro",
+    "deparment": "Architect",
+    "email": "thearchitect@gmail.com",
+    "phone": "09076546542",
+    "role": "Doctor",
+    "faculty": "Sciences"
+  },
+  {
+    "fullName": "Alonso Xabi",
+    "userID": "AlXabi",
+    "deparment": "Computer Science",
+    "email": "xabialonso@gmail.com",
+    "phone": "09065785432",
+    "role": "Lecturer 2",
+    "faculty": "Agriculture"
+  },
+  {
+    "fullName": "Zenedine Zidane",
+    "userID": "Zizou",
+    "deparment": "Art",
+    "email": "zizouzidane@gmail.com",
+    "phone": "09076546542",
+    "role": "Doctor",
+    "faculty": "Agriculture" 
+  },
+  {
+    "fullName": "Andrea Pirlo",
+    "userID": "Maestro",
+    "deparment": "Architect",
+    "email": "thearchitect@gmail.com",
+    "phone": "09076546542",
+    "role": "Doctor",
+    "faculty": "Sciences" 
+  },
+  {
+    "fullName": "Andreas Iniesta",
+    "userID": "Maestro",
+    "deparment": "Architect",
+    "email": "thearchitect@gmail.com",
+    "phone": "09076546542",
+    "role": "Docter",
+    "faculty": "Sciences"
+  }
 ]
 
 export default LecturerList
--- src/components/Lecturer/LecturerList/ListContent.jsx
+++ src/components/Lecturer/LecturerList/ListContent.jsx
@@ -2,45 +2,57 @@ import React from "react"
 
 import styles from "./stylesTwo.module.css"
 
-const ListContent = () => {
+const ListContent = (props) => {
+
+  const ListItem = (props) => (
+    <div className={styles.lecturerItem} style={{backgroundColor: props.bg}}>
+      <p className={`${styles.elementText} ${styles.paddingLeft}`}>{props.id + 1}</p>
+      <p className={styles.elementText}>{props.data.fullName}</p>
+      <p className={styles.elementText}>{props.data.userID}</p>
+      <p className={styles.elementText}>{props.data.deparment}</p>
+      <p className={styles.elementText}>{props.data.email}</p>
+      <p className={styles.elementText}>{props.data.phone}</p>
+      <p className={styles.elementText}>{props.data.role}</p>
+    </div>
+  )
+
   return (
     <div className={styles.container}>
       <div className={styles.testTwo}>
-        <div className={styles.red}>
-
+        <div className={styles.headerContainer}>
+          <p className={`${styles.headerText} ${styles.paddingLeft}`}>S/N</p>
+          <p className={styles.headerText}>Full Name</p>
+          <p className={styles.headerText}>User ID</p>
+          <p className={styles.headerText}>Department</p>
+          <p className={styles.headerText}>Email</p>
+          <p className={styles.headerText}>Phone</p>
+          <p className={styles.headerText}>Roles</p>
         </div>
 
-        <div className={styles.blue}>
-
-        </div>
-
-        <div className={styles.brown}>
-
-        </div>
-
-        <div className={styles.pink}>
-
-        </div>
-
-        <div className={styles.red}>
-
-</div>
-
-      <div className={styles.blue}>
-
+        {
+          props.data.map((data, id) => {
+            if (((id + 1) % 2) === 0) {
+              return <ListItem bg="#FAFAFA" key={id} data={data} id={id}/>
+            } else {
+              return <ListItem bg="#FFFFFF" key={id} data={data} id={id} />
+            }
+          })
+        }
       </div>
-
-      <div className={styles.brown}>
-
-      </div>
-
-      <div className={styles.pink}>
-
-      </div>
-      </div>
-
     </div>
   )
 }
 
 export default ListContent
+
+
+{/*
+    {
+    "fullName": "Zenedine Zidane",
+    "userID": "Zizou",
+    "deparment": "Art",
+    "email": "zizouzidane@gmail.com",
+    "phone": "09076546542",
+    "role": "Docter" 
+  }, 
+*/}
